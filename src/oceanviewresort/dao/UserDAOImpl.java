b609a6da9067ff5539b1ebac28f154454111d6f7
AadilAR <aadilar2@gmail.com>
2026-02-07 21:44:41 +0530

    Implemented JDBC logic for user authentication

--- src/oceanviewresort/dao/UserDAOImpl.java
+++ src/oceanviewresort/dao/UserDAOImpl.java
@@ -1,17 +1,59 @@
 package oceanviewresort.dao;
 
 import oceanviewresort.model.User;
+import oceanviewresort.util.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDAOImpl implements UserDAO {
 
     @Override
     public User findByUsername(String username) {
-        // TODO: Implement JDBC logic
-        return null;
+
+        String sql = "SELECT * FROM user WHERE username = ?";
+        User user = null;
+
+        try (Connection conn = DBUtil.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                user = new User(
+                        rs.getInt("user_id"),
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("role")
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
     }
 
     @Override
     public void addUser(User user) {
-        // TODO: Implement JDBC logic
+
+        String sql = "INSERT INTO user (username, password, role) VALUES (?, ?, ?)";
+
+        try (Connection conn = DBUtil.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getPassword());
+            stmt.setString(3, user.getRole());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
